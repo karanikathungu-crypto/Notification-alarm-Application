5a0667ff6fb5e16a3b79dc408eecea31ae15a266
karanikathungu-crypto <kathungudev@gmail.com>
2022-01-14 21:05:37 +0300

    "Create the methods for selecting time setting and cancelling alarm"

--- app/src/main/java/com/kenneth/myalarm/MainActivity.java
+++ app/src/main/java/com/kenneth/myalarm/MainActivity.java
@@ -57,7 +57,60 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    private void setAlarm(){
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this,AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0 ,intent, 0);
+        alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY, pendingIntent);
 
+        Toast.makeText(this, "Alarm set successfully", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void cancelAlarm() {
+        Intent intent = new Intent(this,AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 0 ,intent, 0);
+
+        if(alarmManager == null){
+            alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        }
+        alarmManager.cancel(pendingIntent);
+        Toast.makeText(this, "Alarm Cancelled", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void showTimePicker(){
+
+        picker = new MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(12)
+                .setMinute(0)
+                .setTitleText("Select Alarm Time")
+                .build();
+
+        picker.show(getSupportFragmentManager(), "fosandroid");
+        picker.addOnPositiveButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (picker.getHour() > 12) {
+                    binding.selectedTime.setText(
+                            String.format("%02d", (picker.getHour()-12))+" : " + String.format("%02d", picker.getMinute())+ " PM");
+                }
+                else {
+                    binding.selectedTime.setText(picker.getHour()+":"+picker.getMinute() + "AM");
+                }
+
+                calendar = Calendar.getInstance();
+                calendar.set(Calendar.HOUR_OF_DAY,picker.getHour());
+                calendar.set(Calendar.MINUTE,picker.getMinute());
+                calendar.set(Calendar.SECOND,0);
+                calendar.set(Calendar.MILLISECOND,0);
+            }
+        });
+
+    }
 
     private void createnotificationchannel() {
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){

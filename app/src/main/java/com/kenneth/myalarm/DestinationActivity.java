1cab8a7db5ca46cc94a3f11add7fab21f36f3401
karanikathungu-crypto <kathungudev@gmail.com>
2022-01-14 21:06:11 +0300

    "Create the destination activity for later use"

---
+++ app/src/main/java/com/kenneth/myalarm/DestinationActivity.java
@@ -0,0 +1,14 @@
+package com.kenneth.myalarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DestinationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination);
+    }
+}
